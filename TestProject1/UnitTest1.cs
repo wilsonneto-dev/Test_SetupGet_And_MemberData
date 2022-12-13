113ac2d44b0c9c9e90ac0cadeb53691cdc4b8188
Wilson Neto B.R <contato@wilsonneto.com.br>
2022-12-13 18:32:26 -0300

    feat: New tests

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,27 +1,91 @@
 using FluentAssertions;
+using FluentValidation;
 using Moq;
-using System.Linq.Expressions;
 
 namespace TestProject1;
 
+public class UnitTest01
+{
+    [Theory,
+        MemberData(memberName: nameof(GetProperties))]
+    public void Test1(Func<Mock<IPayload>, Moq.Language.Flow.IReturnsResult<IPayload>> setup, string expectedMessage)
+    {
+        var payload = PayloadBuilder.Build();
+        setup(Mock.Get(payload));
+        var sut = new PayloadValidator();
+
+        var validationResult = sut.Validate(payload);
+
+        validationResult.IsValid.Should().BeFalse();
+        validationResult.Errors.First().ErrorMessage.Should().Be(expectedMessage);
+    }
+
+    public static IEnumerable<object[]> GetProperties() => new List<object[]>(){
+        new object[] {
+            (Mock<IPayload> mock) => mock.SetupGet(x => x.Name).Returns(""),
+            "'Name' must not be empty."
+        },
+        new object[] {
+            (Mock<IPayload> mock) => mock.SetupGet(x => x.LastName).Returns(""),
+            "'Last Name' must not be empty."
+        },
+        new object[] {
+            (Mock<IPayload> mock) => mock.SetupGet(x => x.Number).Returns(0),
+            "'Number' must be greater than '0'."
+        }
+    };
+}
+
+public class UnitTest02
+{
+    [Theory,
+        MemberData(memberName: nameof(GetProperties))]
+    public void Test1(IPayload payload, string expectedMessage)
+    {
+        var sut = new PayloadValidator();
+
+        var validationResult = sut.Validate(payload);
+
+        validationResult.IsValid.Should().BeFalse();
+        validationResult.Errors.First().ErrorMessage.Should().Be(expectedMessage);
+    }
+
+    public static IEnumerable<object[]> GetProperties()
+    {
+        var payload = PayloadBuilder.Build();
+        Mock.Get(payload).SetupGet(x => x.Name).Returns("");
+        yield return new object[] { payload, "'Name' must not be empty." };
+
+        payload = PayloadBuilder.Build();
+        Mock.Get(payload).SetupGet(x => x.LastName).Returns("");
+        yield return new object[] { payload, "'Last Name' must not be empty." };
+
+        payload = PayloadBuilder.Build();
+        Mock.Get(payload).SetupGet(x => x.Number).Returns(0);
+        yield return new object[] { payload, "'Number' must be greater than '0'." };
+    }
+}
 public interface IPayload
 {
     string Name { get; }
     string LastName { get; }
+    int Number { get; }
 }
 
-public class UnitTest1
+static class PayloadBuilder
 {
-    [Theory,
-        MemberData(memberName: nameof(GetProperties))]
-    public void Test1(Func<IPayload, string> lambda,string value, string expected)
-    {
-        var p = Mock.Of<IPayload>();
-        Mock.Get(p).SetupGet(lambda).Returns(value);
-        p.Name.Should().Be(expected);
-    }
+    public static IPayload Build() => Mock.Of<IPayload>(
+        x => x.Name == "Wilson" &&
+        x.LastName == "Gomes" &&
+        x.Number == 10);
+}
 
-    public static IEnumerable<object[]> GetProperties() => new List<object[]>(){
-        new object[] { (IPayload x) => x.Name, "teste", "teste" }
-    };
+class PayloadValidator : AbstractValidator<IPayload>
+{
+    public PayloadValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty();
+        RuleFor(x => x.LastName).NotEmpty();
+        RuleFor(x => x.Number).GreaterThan(0);
+    }
 }
